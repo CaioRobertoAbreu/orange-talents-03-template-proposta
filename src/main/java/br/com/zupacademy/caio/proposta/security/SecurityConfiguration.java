2b2504699190cfea2169fcec9323d5945269bdf8
caiorobertoabreu <caio3015@hotmail.com>
2021-04-27 14:33:51 -0300

    Login via senha

---
+++ src/main/java/br/com/zupacademy/caio/proposta/security/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package br.com.zupacademy.caio.proposta.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/proposta").hasAnyAuthority("SCOPE_teste")
+                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                .and()
+                .csrf()
+                .and()
+                .cors().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
